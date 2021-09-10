5d8734a89ecfe26609dfefc64ed700548c3cf6e5
Usama Javed <ujaved007@gmail.com>
2021-09-11 04:26:20 +1000

    Error message fix for non-ints

--- app.js
+++ app.js
@@ -49,21 +49,29 @@ catAgeField.addEventListener("click", () => {
 //cat calculator
 
 catAgeCalcBtn.addEventListener("click", () => {
-	const catAgeFieldValue = parseInt(catAgeField.value);
-	if (catAgeFieldValue <= 2 && catAgeFieldValue > 0) {
-		if (catAgeFieldValue === 1) {
-			return (catResult.textContent = "Your cat is 15 years old");
-		} else return (catResult.textContent = "Your cat is 24 years old");
-	} else if (catAgeFieldValue > 2) {
-		return (catResult.textContent = `Your cat is ${
-			24 + (catAgeFieldValue - 2) * 4
-		} years old`);
-	} else
+	const catAgeFieldValue = Number(catAgeField.value);
+	if (!Number.isInteger(catAgeFieldValue) || catAgeFieldValue <= 0) {
 		return (catResult.textContent =
-			"Please enter a valid number greater than zero");
+			"Please enter a valid number greater than zero without decimals");
+	} else {
+		if (catAgeFieldValue <= 2 && catAgeFieldValue > 0) {
+			if (catAgeFieldValue === 1) {
+				return (catResult.textContent = "Your cat is 15 years old");
+			} else return (catResult.textContent = "Your cat is 24 years old");
+		} else if (catAgeFieldValue > 2) {
+			return (catResult.textContent = `Your cat is ${
+				24 + (catAgeFieldValue - 2) * 4
+			} years old`);
+		}
+	}
 });
 
 //DOGCALCULATOR
+
+function dogError() {
+	return (dogResult.textContent =
+		"Please enter a valid number greater than zero without decimals");
+}
 
 function dogAgeUntilTwo(age) {
 	if (age === 1) {
@@ -76,69 +84,79 @@ function dogAgeUntilTwo(age) {
 }
 
 function smBreed(age) {
-	if (age <= 2) {
-		return dogAgeUntilTwo(age);
+	if (!Number.isInteger(age) || age <= 0) {
+		return dogError();
 	} else {
-		return (dogResult.textContent = `Your dog is ${
-			24 + (age - 2) * 4
-		} years old`);
+		if (age <= 2) {
+			return dogAgeUntilTwo(age);
+		} else {
+			return (dogResult.textContent = `Your dog is ${
+				24 + (age - 2) * 4
+			} years old`);
+		}
 	}
 }
 
 function medBreed(age) {
-	if (age <= 2) {
-		return dogAgeUntilTwo(age);
-	} else if (age > 2 && age <= 5) {
-		return (dogResult.textContent = `Your dog is ${
-			24 + (age - 2) * 4
-		} years old`);
-	} else if (age > 5) {
-		if (age % 2 !== 0) {
+	if (!Number.isInteger(age) || age <= 0) {
+		return dogError();
+	} else {
+		if (age <= 2) {
+			return dogAgeUntilTwo(age);
+		} else if (age > 2 && age <= 5) {
 			return (dogResult.textContent = `Your dog is ${
-				38 + (Math.floor(age / 2) - 2) * 9
+				24 + (age - 2) * 4
 			} years old`);
-		} else {
-			return (dogResult.textContent = `Your dog is ${
-				42 + (age / 2 - 3) * 9
-			} years old`);
+		} else if (age > 5) {
+			if (age % 2 !== 0) {
+				return (dogResult.textContent = `Your dog is ${
+					38 + (Math.floor(age / 2) - 2) * 9
+				} years old`);
+			} else {
+				return (dogResult.textContent = `Your dog is ${
+					42 + (age / 2 - 3) * 9
+				} years old`);
+			}
 		}
 	}
 }
-
 function lgeBreed(age) {
-	if (age <= 2) {
-		return dogAgeUntilTwo(age);
-	} else if (age > 2 && age <= 5) {
-		return (dogResult.textContent = `Your dog is ${
-			24 + (age - 2) * 4
-		} years old`);
-	} else if (age > 5 && age <= 8) {
-		return (dogResult.textContent = `Your dog is ${
-			45 + (age - 6) * 5
-		} years old`);
-	} else if (age > 8 && age <= 12) {
-		if (age % 2 !== 0) {
+	if (!Number.isInteger(age) || age <= 0) {
+		return dogError();
+	} else {
+		if (age <= 2) {
+			return dogAgeUntilTwo(age);
+		} else if (age > 2 && age <= 5) {
 			return (dogResult.textContent = `Your dog is ${
-				61 + (Math.floor(age / 2) - 4) * 11
+				24 + (age - 2) * 4
 			} years old`);
-		} else {
+		} else if (age > 5 && age <= 8) {
 			return (dogResult.textContent = `Your dog is ${
-				66 + (age / 2 - 5) * 11
+				45 + (age - 6) * 5
 			} years old`);
-		}
-	} else if (age >= 13) {
-		if (age % 2 !== 0) {
-			return (dogResult.textContent = `Your dog is ${
-				82 + (Math.floor(age / 2) - 6) * 11
-			} years old`);
-		} else {
-			return (dogResult.textContent = `Your dog is ${
-				88 + (age / 2 - 7) * 11
-			} years old`);
+		} else if (age > 8 && age <= 12) {
+			if (age % 2 !== 0) {
+				return (dogResult.textContent = `Your dog is ${
+					61 + (Math.floor(age / 2) - 4) * 11
+				} years old`);
+			} else {
+				return (dogResult.textContent = `Your dog is ${
+					66 + (age / 2 - 5) * 11
+				} years old`);
+			}
+		} else if (age >= 13) {
+			if (age % 2 !== 0) {
+				return (dogResult.textContent = `Your dog is ${
+					82 + (Math.floor(age / 2) - 6) * 11
+				} years old`);
+			} else {
+				return (dogResult.textContent = `Your dog is ${
+					88 + (age / 2 - 7) * 11
+				} years old`);
+			}
 		}
 	}
 }
-
 // toggle results DOGS
 
 dogAgeField.addEventListener("click", () => {
@@ -167,7 +185,7 @@ largeBreedBtn.addEventListener("click", () => {
 });
 
 dogAgeCalcBtn.addEventListener("click", () => {
-	const dogAgeFieldValue = parseInt(dogAgeField.value);
+	const dogAgeFieldValue = Number(dogAgeField.value);
 	if (smallBreedBtn.classList.contains("selected-btn")) {
 		smBreed(dogAgeFieldValue);
 	} else {
